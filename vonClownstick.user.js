c3e52e06a9f6c93eee3079b5042e5cf6e1d2697e
frankgajda <frank@gajda.dk>
2017-01-11 13:37:36 +0100

    Initial version

---
+++ vonClownstick.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         vonClownstick
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Making news less depressing!
+// @author       Frank M.G. Jørgensen
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var replaceArray = [
+        [/@realdonaldtrump/gi,  '@shitforbrains'],
+        [/Donald J. Trump/gi,   'Farty McFuckface'],
+        [/Donald Trump/gi,    'Fuckface von Clownstick'],
+        [/President Trump/gi,          'President Lying McBastard'],
+        [/Trump Tower/gi,  'Golden Shower Tower'],
+        [/Trump/gi,            'von Clownstick'],
+        [/Melania Trump/gi,  'Goldigia von Clownstick'],
+        // etc.
+    ];
+    var numTerms    = replaceArray.length;
+    var txtWalker   = document.createTreeWalker (
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        {   acceptNode: function (node) {
+            //-- Skip whitespace-only nodes
+            if (node.nodeValue.trim() )
+                return NodeFilter.FILTER_ACCEPT;
+
+            return NodeFilter.FILTER_SKIP;
+        }
+        },
+        false
+    );
+    var txtNode     = null;
+
+    while (txtNode  = txtWalker.nextNode () ) {
+        var oldTxt  = txtNode.nodeValue;
+
+        for (var i  = 0;  i < numTerms;  i++) {
+            oldTxt  = oldTxt.replace (replaceArray[i][0], replaceArray[i][1]);
+        }
+        txtNode.nodeValue = oldTxt;
+    }
+})();
